d80e507440dbf92e83058273d71320fcd06defbf
Raquel Plaza <>
2018-06-08 17:25:46 +0100

    added manage course page

---
+++ src/components/course/CourseForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TextInput from '../common/TextInput';
+
+const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
+    return (
+        <form action="">
+            <h1>Manage course</h1>
+            <TextInput></TextInput>
+        </form>
+    )
+}
